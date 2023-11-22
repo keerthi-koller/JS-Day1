d83a234105630d4f4cb3f2fe5de5b2c6c5bed317
keerthi-koller <keerthi.nk8@gmail.com>
2023-11-22 12:56:58 +0530

    added

---
+++ Reverse.js
@@ -0,0 +1,8 @@
+let str = "Keerthi";
+let s = "";
+for (let i=0; i<=str.length-1; i++)
+{
+    s = str.charAt(i) + s;
+}
+
+console.log(s);
---
+++ SumArray.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 50];
+
+let sum = 0;
+for (let i=0; i<=arr.length-1; i++)
+{
+    sum = sum + arr[i];
+}
+
+console.log(sum);
